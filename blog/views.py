d88b7484dfa59ef33be77c068ff0177118175f0d
test8140 <test8140@ukr.net>
2020-07-12 15:04:37 +0300

    Transferring a template to a project.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse('<h1>Hello Dimon</h1>')
+    return render(request, 'blog/index.html')
--- siteblog/settings.py
+++ siteblog/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'siteblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'siteblog/static'),
+]
