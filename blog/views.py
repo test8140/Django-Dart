4003697e4518a44ce05526105700f31f65015f57
test8140 <test8140@ukr.net>
2020-08-08 16:46:11 +0300

    Display articles by tag.

--- blog/views.py
+++ blog/views.py
@@ -34,10 +34,20 @@ class PostsByCategory(ListView):
 
 
 class PostsByTag(ListView):
-   pass
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug = self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Запись по тегу: ' + str(Tag.objects.get(slug = self.kwargs['slug']))
+        return context
 
 
-    
 class GetPost(DetailView):
     model = Post
     template_name = 'blog/single.html'
